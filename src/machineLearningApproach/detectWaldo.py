dae32e00020a9aa27e412cf5683cc132ae67eb0f
ASWANTH P P <ppaswanth3@gmail.com>
2018-03-07 00:45:40 +0530

    source code modified

--- src/machineLearningApproach/detectWaldo.py
+++ src/machineLearningApproach/detectWaldo.py
@@ -1,17 +1,15 @@
 from matplotlib import pyplot as plt
 import numpy as np
-import sys
-import tensorflow as tf
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+import sys,tensorflow as tf
 import matplotlib
 from PIL import Image
 import matplotlib.patches as patches
-from object_detection.utils import label_map_util
+import cv2
 from object_detection.utils import visualization_utils as vis_util
-
-
 model_path = 'frozen_inference_graph.pb'
 image_path='input.jpg'
-
 detection_graph = tf.Graph()
 with detection_graph.as_default():
     od_graph_def = tf.GraphDef()
@@ -19,17 +17,10 @@ with detection_graph.as_default():
         serialized_graph = fid.read()
         od_graph_def.ParseFromString(serialized_graph)
         tf.import_graph_def(od_graph_def, name='')
-
 def load_image_into_numpy_array(image):
   (im_width, im_height) = image.size
-  return np.array(image.getdata()).reshape(
-      (im_height, im_width, 3)).astype(np.uint8)
-
-
-
-category_index={'name':"waldo",'id':1}
-
-	
+  return np.array(image.getdata()).reshape((im_height, im_width, 3)).astype(np.uint8)
+category_index={'name':"wald	o",'id':1}
 with detection_graph.as_default():
   with tf.Session(graph=detection_graph) as sess:
     image_np = load_image_into_numpy_array(Image.open(image_path))
@@ -38,23 +29,19 @@ with detection_graph.as_default():
     scores = detection_graph.get_tensor_by_name('detection_scores:0')
     classes = detection_graph.get_tensor_by_name('detection_classes:0')
     num_detections = detection_graph.get_tensor_by_name('num_detections:0')
-    # Actual detection.
     (boxes, scores, classes, num_detections) = sess.run(
         [boxes, scores, classes, num_detections],
         feed_dict={image_tensor: np.expand_dims(image_np, axis=0)})
-
     if scores[0][0] < 0.1:
-        sys.exit('Wally not found :(')
-
-    print('Wally found')
-    vis_util.visualize_boxes_and_labels_on_image_array(
-        image_np,
-        np.squeeze(boxes),
+        sys.exit('Waldo not found :(')
+    print('Waldo found')
+    vis_util.visualize_boxes_and_labels_on_image_array(image_np,np.squeeze(boxes),
         np.squeeze(classes).astype(np.int32),
         np.squeeze(scores),
         category_index,
         use_normalized_coordinates=True,
         line_thickness=8)
-    plt.figure(figsize=(12, 8))
-    plt.imshow(image_np)
-    plt.show()
+    #plt.figure(figsize=(24, 16))
+    #plt.imshow(image_np)
+    #plt.show()
+    cv2.imwrite("output.png",image_np)
